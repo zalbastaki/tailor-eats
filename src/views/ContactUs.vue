955e7387c1e0904e1885467785e953279f4d2d17
Zain Albastaki <zain.albastaki@gmail.com>
2021-03-13 12:22:00 +0300

    Make contact us messageSent default value false

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -73,7 +73,7 @@
                 name: '',
                 email: '',
                 message: '',
-                messageSent: true,
+                messageSent: false,
             };
         },
 
